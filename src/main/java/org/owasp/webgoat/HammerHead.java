14d914eaa26be1897f4f918849c22b2c03b41d44
devopsadmin12 <94431252+devopsadmin12@users.noreply.github.com>
2022-06-14 14:26:13 +0530

    Update HammerHead.java

--- src/main/java/org/owasp/webgoat/HammerHead.java
+++ src/main/java/org/owasp/webgoat/HammerHead.java
@@ -45,7 +45,7 @@ import org.slf4j.LoggerFactory;
  *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Place - Suite 330, Boston, MA 02111-1307, U
  *
  * Getting Source ==============
  *
